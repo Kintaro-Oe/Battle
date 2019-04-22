3d0cc7f87a9c71a4bef5ba9f13f8c35f00bb05a1
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-22 21:22:01 +0100

    all Game class to accept player instances

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -2,14 +2,26 @@ require 'spec_helper'
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player1) { double :player1 }
-  let(:player2) { double :player2 }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
 
   describe '#attack' do
     it 'damages the player' do
-      expect(player2).to receive(:receive_damage)
-      game.attack(player2)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
+
+  describe '#player_1' do
+    it "stores player 1" do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_1' do
+    it "stores player 2" do
+      expect(game.player_2).to eq player_2
     end
   end
 end
